7f3b9b728c58ef2aa5ddd9e1146d37a3c1380cf0
kj <373474453@qq.com>
2018-12-13 18:24:05 +0800

    提取公共工具类

--- src/main/java/com/algorithm/dispatch/Verify.java
+++ src/main/java/com/algorithm/dispatch/Verify.java
@@ -56,7 +56,7 @@ public class Verify {
             return false;
         }
         if (n == 0) {
-            deepCopy(arrayList, array);
+            DispatchUtils.deepCopy(arrayList, array);
 //            System.out.println("当前验证调度方案：");
 //            for (Byte[] a : arrayList) {
 //                System.out.println(Arrays.toString(a));
@@ -68,7 +68,7 @@ public class Verify {
             for (int i = 0; i < arrayList[0].length + 1; i++) {
                 if (i > 0) {
                     arrayList = array;
-                    arrayList[n] = moveArrayElement(arrayList[n], i);
+                    arrayList[n] = DispatchUtils.moveArrayElement(arrayList[n], i);
                 }
                 if (i == arrayList[0].length) {
 //                    System.out.println("完成第" + (n + 1) + "层");
@@ -83,7 +83,7 @@ public class Verify {
             for (int i = 0; i < arrayList[0].length + 1; i++) {
                 if (i > 0) {
                     arrayList = array;
-                    arrayList[n] = moveArrayElement(arrayList[n], i);
+                    arrayList[n] = DispatchUtils.moveArrayElement(arrayList[n], i);
                 }
                 if (i == arrayList[0].length) {
 //                    System.out.println("完成最内层");
@@ -110,23 +110,6 @@ public class Verify {
         return true;
     }
 
-    private static Byte[] moveArrayElement(Byte[] array, int k) {
-        if (k == 0) {
-            return array;
-        }
-        int length = array.length;
-        // 右移newk + n * length个位置，和右移newk个位置效果是一样的
-        int newk = k % length;
-        Byte[] newArray = new Byte[length];
-        // 重复length次把元素从旧位置移到新位置
-        for (int i = 0; i < length; i++) {
-            // 求出元素新的位置
-            int newPosition = (i + newk) % length;
-            newArray[newPosition] = array[i];
-        }
-        return newArray;
-    }
-
     public void nextArray(Byte[][] arrayList, int i) {
         if (arrayList.length == 0) {
             return;
@@ -138,29 +121,9 @@ public class Verify {
             flag = verify(arrayList, 0);
             if (flag) {
                 Byte[][] copy = new Byte[arrayList.length][arrayList[0].length];
-                deepCopy(arrayList, copy);
+                DispatchUtils.deepCopy(arrayList, copy);
                 fit.add(copy);
             }
-        }
-    }
-
-    public static Byte[][] initArray(int n, int t) {
-        Byte[][] arrayList = new Byte[n][t];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < t; j++) {
-                if (j == 0) {
-                    arrayList[i][j] = 1;
-                } else {
-                    arrayList[i][j] = 0;
-                }
-            }
-        }
-        return arrayList;
-    }
-
-    private static void deepCopy(Byte[][] copyFrom, Byte[][] copyTo) {
-        for (int i = 0; i < copyFrom.length; i++) {
-            copyTo[i] = copyFrom[i].clone();
         }
     }
 }
